7a54b6d4f313814116074d24e03fa8757b8b3aa1
Frederico Gregório <fred.engcomp@gmail.com>
2025-09-03 16:51:01 -0300

    atualçização[

--- helloworld.c
+++ helloworld.c
@@ -5,9 +5,13 @@
 
 int main( int argc, vhar *argv[]) {
 
-    int a= atoi(argv[a])
-    int b = atoi(argv[2]);
 
+    
+    int a= atoi(argv[1])
+    int b = atoi(argv[2]);
+   
+
+    printf("Seu resultado: ", a + b);
 
 
 
