27f6791d8a70c4641bde80a667bdd66d650ad29d
Frederico Gregório <fred.engcomp@gmail.com>
2025-09-03 16:58:56 -0300

    atualziação

---
+++ Unidade 1/q01.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdio.h>
+
+//imprimir Hello World
+
+int main(int argc, vhar *argv[]){
+
+rintf("Hello World");
+
+    return 0;
+}
---
+++ Unidade 1/q02.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdio.h>
+
+//  fazer uma soma de 2 numeros e imprimirs
+
+int main(int argc, vhar *argv[]){
+
+rintf("Hello World");
+
+    return 0;
+}
